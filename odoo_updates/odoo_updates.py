c9792c371b4149c252deb0e2669bfeeb6b91c571
angelfentanez <joseangel@vauxoo.com>
2016-05-23 19:17:33 -0500

    [FIX] odoo_updates: use correct key for the format and use secho to display the path in yello

--- odoo_updates/odoo_updates.py
+++ odoo_updates/odoo_updates.py
@@ -191,9 +191,9 @@ def diff_to_screen(views_states, title):
 def branches_to_screen(branches):
     click.echo('Repositories:\n')
     for branch in branches:
-        click.echo('{path}'.format(path=branch['path'], fg='yellow'))
+        click.secho('{path}'.format(path=branch['path']), fg='yellow')
         click.echo('{repo} {branch}'.format(branch=branch['branch'], repo=branch['name']))
-        click.echo('{commit}'.format(path=branch['commit']))
+        click.echo('commit: {commit}'.format(commit=branch['commit']))
         for remote, url in branch['repo_url'].iteritems():
             click.echo('{remote}: {url}'.format(url=url, remote=remote))
         click.echo('\n')
